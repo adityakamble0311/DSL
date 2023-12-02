7331efef7328ff0052fd3308adede843e91675e6
Aditya Kamble <129869551+adityakamble0311@users.noreply.github.com>
2023-12-03 01:29:58 +0530

    Create longest_number_count.py

---
+++ longest_number_count.py
@@ -0,0 +1,8 @@
+def display_word_with_longest_length():
+    string_input = input("Enter the main string: ")
+    words = string_input.split()
+    longest_word = max(words, key=len, default="")
+
+    print("\tWord with the longest length is {} having length {}\n\n".format(longest_word, len(longest_word)))
+
+display_word_with_longest_length()
